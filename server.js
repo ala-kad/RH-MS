8495501d062fa8af50d8e2977dfcf315233915b4
ala-kad <kaddechiala@gmail.com>
2022-08-26 16:25:18 +0100

    CRUD on projects models

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-// const mongoose = require('mongoose');
+// requiring connection to Db file
 const connectDB = require('./db/connect');
+// requiring member model
 const member = require('./models/members');
+// requiring projects model
+const project = require('./models/projects');
 
 // requiring and accessing .env variables 
 require('dotenv').config();
-
 // share public folder and make it accessible 
 // app.use(express.static('./public'))
 app.use(express.json()) // if we don't use this then we win't have that data in req.body
@@ -14,36 +16,37 @@ app.use(express.json()) // if we don't use this then we win't have that data in 
 app.get('/', (req,res) => {
     res.send('Welcome to our app ! ');
 })
+
+// CRUD Operations on members
 // POST method : adding a new member 
 app.post('/add', (req,res) => {
-    console.log('Post is working')
     data = req.body;
     console.log(data)
     const Member = new member(data)
     Member.save()
     .then(
         (savedMember) => {
-            console.log(savedMember)
+            res.status(200).send(savedMember)
+            console.log('just added a new member!')
         }
     )
     .catch (
-        (err) => {
-            console.logg (err)
+    (err) => {
+        res.status(400).send(err)  
     })
     /* res.send(__dirname + './public/index.html');*/
 })
-
 // GET method: view all members 
 app.get('/getall', (req, res) => {
     member.find()
     .then(
         (members)=>{
-            res.send(members)
+            res.status(200).send(members)
         }
     )
     .catch(
         (err) => {
-            res.send(err)
+            res.status(400).send(err)
         }
     )
 })
@@ -52,10 +55,10 @@ app.get('/getbyid/:id', async (req, res) =>{
     try{
         myid = req.params.id;
         membre = await member.findOne({ _id : myid })
-        res.send(membre)
+        res.status(200).send(membre)
     }
     catch(error){
-        res.send(error)
+        res.status(400).send(error)
     }
 })
 // DELETE method : delete a member by id
@@ -64,16 +67,111 @@ app.delete('/delete/:id' , (req,res)=>{
     member.findOneAndDelete({ _id:id })
     .then (
         (deletedMember) =>{
-            res.send(deletedMember)
+            res.status(200).send(deletedMember)
             console.log('member deleted !')
         }
     )
     .catch (
         (err) => {
-            res.send(err)
+            res.status(400).send(err)
         }
     )
 })
+// PUT method : update a member by id 
+app.put('/update/:id', (req,res) =>{
+    id = req.params.id
+    newMember = req.body
+    member.findByIdAndUpdate({ _id:id}, newMember )
+     .then(
+        (updatedMember) => {
+            res.status(200).send(updatedMember)
+        }
+     )
+     .catch(
+        (err) => {
+            res.status(400).send(err)
+        }
+     )
+})
+
+// PROJECTS
+// Add a new project
+app.post('/addProject', (req,res) => {
+    data = req.body;
+    console.log(data)
+    const Project = new project(data)
+    Project.save()
+    .then(
+        (savedProject) => {
+            res.status(200).send(savedProject)
+            console.log('just added a new project!')
+        }
+    )
+    .catch (
+    (err) => {
+        res.status(400).send(err)  
+    })
+    /* res.send(__dirname + './public/index.html');*/
+})
+// View all projects
+app.get('/getAllProjects', (req, res) => {
+    project.find()
+    .then(
+        (projects)=>{
+            res.status(200).send(projects)
+        }
+    )
+    .catch(
+        (err) => {
+            res.status(400).send(err)
+        }
+    )
+})
+// Get a project by id
+app.get('/getProjectbyid/:id', async (req, res) =>{
+    try{
+        id = req.params.id;
+        proj = await project.findOne({ _id : id })
+        res.status(200).send(proj)
+    }
+    catch(error){
+        res.status(400).send(error)
+    }
+})
+// DELETE a project by id
+app.delete('/deleteProject/:id' , (req,res)=>{
+    id = req.params.id
+    project.findOneAndDelete({ _id:id })
+    .then (
+        (deletedProject) =>{
+            res.status(200).send(deletedProject)
+            console.log('Project deleted !')
+        }
+    )
+    .catch (
+        (err) => {
+            res.status(400).send(err)
+        }
+    )
+})
+// PUT method : update a project by id 
+app.put('/updateProject/:id', (req,res) =>{
+    id = req.params.id
+    newProject = req.body
+    project.findByIdAndUpdate({ _id:id }, newProject )
+     .then(
+        (updatedProject) => {
+            res.status(200).send(updatedProject)
+        }
+     )
+     .catch(
+        (err) => {
+            res.status(400).send(err)
+        }
+     )
+})
+
+
 // Creating a local server on port 3000
 async function start() {
     try {
@@ -85,26 +183,7 @@ async function start() {
         console.log(error);
     }
 }
-// PUT method : update a member by id 
-app.put('/update/:id', (req,res) =>{
-    id = req.params.id
-    newMember = req.body
-    member.findByIdAndUpdate({ _id:id}, newMember )
-     .then(
-        (updatedMember) => {
-            res.send(updatedMember)
-        }
-     )
-     .catch(
-        (err) => {
-            res.send(err)
-        }
-     )
-})
 start();
-
-
-
 // main().catch(err => console.log(err));
 // async function main() {
 //   await mongoose.connect('mongodb://localhost:27017/test');
