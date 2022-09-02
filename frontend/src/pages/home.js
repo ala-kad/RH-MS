b7c1db3d68f757bf0b9eb1ec53b27b64907932a0
ala-kad <kaddechiala@gmail.com>
2022-09-02 08:38:59 +0100

    Homepage(members) + link frontend with backend

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Home =  () => {
+    const [members, setMembers] = useState(null)
+    useEffect(() => {
+        const fetchMembers = async () => {
+            const res = await fetch('/members/getAll/')
+            console.log(res)
+            const json = await res.json()
+            if(res.ok){
+                setMembers(json)
+            }
+        }
+        fetchMembers()
+    }, [])
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="members">
+                {members && members.map((members) => (
+                    <p key={members._id}> {members.name} </p>
+                ))}
+            </div>
         </div>
     )
 }
