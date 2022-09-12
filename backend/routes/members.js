ce613c837f1f9ccbe9267e8462a3098e151110d1
ala-kad <kaddechiala@gmail.com>
2022-09-12 23:00:08 +0100

    controller projects + fixs on members

--- backend/routes/members.js
+++ backend/routes/members.js
@@ -8,68 +8,10 @@ const bcrypt = require('bcrypt')
 // importing jwt
 const jwt = require('jsonwebtoken')
 const util = require('util')
-
-// CRUD Operations on members
-// POST method : adding a new member 
-// router.post('/add', (req,res) => {
-//     data = req.body;
-//     console.log(data)
-//     const member = new Member(data)
-//     Member.save()
-//     .then(
-//         (savedMember) => {
-//             res.status(200).send(savedMember)
-//             console.log('just added a new member!')
-//         }
-//     )
-//     .catch (
-//     (err) => {
-//         res.status(400).send(err)  
-//     })
-//     /* res.send(__dirname + './public/index.html');*/
-// })
-
-// POST method : login
+// POST method : register add a new member
 router.post('/register', signUpMember)
-//     data = req.body
-//     member = new Member(data)
-//     salt = bcrypt.genSaltSync(10);
-//     cryptedPass = await bcrypt.hashSync(data.password, salt)
-//     member.password = cryptedPass
-//     member.save()
-//     .then(
-//         (saved) => {
-//             res.status(200).send(saved)
-//         }
-//     )
-//     .catch(
-//         (err) => {
-//             res.status(400).send(err)
-//         }
-//     )
-// })
 // login function 
 router.post('/login',loginMember)
-//     data = req.body 
-//     member = await Member.findOne({ email : data.email}) 
-//     if(!member) {
-//         res.status(404).send('email or password invalid ')
-//     }else{
-//         validPass = bcrypt.compareSync(data.password, member.password)
-//         if(!validPass){
-//             res.status(401).send('email or password invalid')
-//         }else{
-//             payload = {
-//                 _id: member._id,
-//                 email: member.email,
-//                 name: member.name
-//             }
-//             token = jwt.sign(payload, '123')
-//             res.status(200).send({ mytoken: token })
-//         }
-//     }
-// })
-
 // GET method: view all members 
 router.get('/getall',getMembers) 
 // GET method : find a member by id
@@ -89,5 +31,5 @@ router.get('/getbyid/:id', async (req, res) =>{
 router.put('/update/:id', updateMember)
 // DELETE method : delete a member by id
 router.delete('/delete/:id' , deleteMember)
-module.exports = router 
 // exporting router
+module.exports = router 
--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -2,82 +2,26 @@ const express = require('express')
 const router = express.Router()
 // requiring projects model
 const project = require('../models/projects');
-
+const {addProject,getProjects,updateProject,deleteProject} = require('../controllers/projectsController')
 // PROJECTS CRUD
 // Add a new project
-router.post('/add', (req,res) => {
-    data = req.body;
-    console.log(data)
-    const Project = new project(data)
-    Project.save()
-    .then(
-        (savedProject) => {
-            res.status(200).send(savedProject)
-            console.log('just added a new project!')
-        }
-    )
-    .catch (
-    (err) => {
-        res.status(400).send(err)  
-    })
-    /* res.send(__dirname + './public/index.html');*/
-})
+router.post('/add', addProject)
 // View all projects
-router.get('/getAll', (req, res) => {
-    project.find()
-    .then(
-        (projects)=>{
-            res.status(200).send(projects)
-        }
-    )
-    .catch(
-        (err) => {
-            res.status(400).send(err)
-        }
-    )
-})
+router.get('/getAll', getProjects)
 // Get a project by id
-router.get('/getbyid/:id', async (req, res) =>{
-    try{
-        id = req.params.id;
-        proj = await project.findOne({ _id : id })
-        res.status(200).send(proj)
-    }
-    catch(error){
-        res.status(400).send(error)
-    }
-})
+// router.get('/getbyid/:id', async (req, res) =>{
+//     try{
+//         id = req.params.id;
+//         proj = await project.findOne({ _id : id })
+//         res.status(200).send(proj)
+//     }
+//     catch(error){
+//         res.status(400).send(error)
+//     }
+// })
 // DELETE a project by id
-router.delete('/delete/:id' , (req,res)=>{
-    id = req.params.id
-    project.findOneAndDelete({ _id:id })
-    .then (
-        (deletedProject) =>{
-            res.status(200).send(deletedProject)
-            console.log('Project deleted !')
-        }
-    )
-    .catch (
-        (err) => {
-            res.status(400).send(err)
-        }
-    )
-})
+router.delete('/delete/:id' , deleteProject)
 // PUT method : update a project by id 
-router.put('/update/:id', (req,res) =>{
-    id = req.params.id
-    newProject = req.body
-    project.findByIdAndUpdate({ _id:id }, newProject )
-     .then(
-        (updatedProject) => {
-            res.status(200).send(updatedProject)
-        }
-     )
-     .catch(
-        (err) => {
-            res.status(400).send(err)
-        }
-     )
-})
+router.put('/update/:id', updateProject)
+// exporting projetcs router as a module
 module.exports = router;
-// exporting router as a module
